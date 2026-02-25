296187a15bd1678773149eff9efa69fbe6bee9ae
nazarit19 <138739227+nazarit19@users.noreply.github.com>
2026-02-25 10:38:03 -0600

    changed port

--- photogallery/app.py
+++ photogallery/app.py
@@ -247,4 +247,4 @@ def my_search_page():
                            username=session.get('username'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5000)
